5c649b272b65107c49e51819ab3f55807d5bf6ba
Hassam Amer <hasideveloper@gmail.com>
2024-05-09 22:28:30 +0500

    Started Basic Project to understand Redux-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar"
+import UserDetails from "./Components/UserDetails"
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <UserDetails></UserDetails>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/DeleteAllUser.jsx
@@ -0,0 +1,11 @@
+
+
+const DeleteAllUser = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DeleteAllUser
